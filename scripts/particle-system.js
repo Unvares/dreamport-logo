dcae1ee048106071924dbc28e38aa0f96061275d
Unvares <okmanis8@gmail.com>
2021-09-01 12:29:19 +0500

    Wrote dev-instruction

---
+++ scripts/particle-system.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/* 
+    Time Tracker: 1.5 hours
+
+    Minimum Viable Functionality:
+    - You can define particle lifetime
+    - You can define particle flight direction
+    - You can define particle flight speed
+    - You can randomise all parameters written above
+    - You can define particle texture
+    - Particle system is implemented into A-Frame as a component
+
+    Questions to answer:
+    - Each ParticleSystem class is for creating and adjusting one particle or bunch of particles?
+    - Which methods and variables are intended for incapsulation and which are not? How to incapsulate them?
+
+    Goals for the next week:
+    - Implement at least 4 points from the MVF list
+*/
